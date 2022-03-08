7a64a32aa61247915da12e88c4a233edd2753069
Oscar Canedo <oscar.canedo@bootcamp-upgrade.com>
2022-03-09 00:13:59 +0100

    Iteration#4

--- index.js
+++ index.js
@@ -148,3 +148,32 @@ const users = [
   }
   
   console.log(favoritesSounds);
+
+  // Iteration 4
+
+  /* Crea una función llamada findArrayIndex que reciba como parámetros un array de textos y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parámetro */
+
+  function findArrayIndex(array, text) {
+    
+    for (let i = 0; i < array.length; i++) {
+      
+    let obj = array[i];
+    
+    if (obj === text) {
+    
+        return i;
+      }
+    }
+  }
+  
+  const index = findArrayIndex(
+    ["Caracol", "Mosquito", "Salamandra", "Ajolote"],
+    "Ajolote"
+  );
+  console.log(index);
+  
+  const index2 = findArrayIndex(
+    ["Caracol", "Mosquito", "Salamandra", "Ajolote"],
+    "Mosquito"
+  );
+  console.log(index2);
